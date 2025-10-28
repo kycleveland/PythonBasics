8583320efeefbc1612ea1d4f55291c9f6a374435
Kyle Cleveland <kycleveland@gmail.com>
2025-10-28 05:20:41 -0500

    Created a basic FOR loop script

---
+++ ForLoops.py
@@ -0,0 +1,25 @@
+favorite_cars = [
+    {
+        "make": "Telsa",
+        "model": "Model S",
+        "year": 2022,
+        "price": 79999.99,
+        "features": ["Autopilot", "Electric", "All-Wheel Drive"],
+        "is_electric": True,
+        "owner": None
+    },
+    {
+        "make": "Porshe",
+        "model": "911 Carrera",
+        "year": 2023,
+        "price": 104999.99,
+        "features": ["Autopilot", "Electric", "All-Wheel Drive"],
+        "is_electric": False,
+        "owner": None
+    }
+]
+
+if any(car['make'] == 'Porshe' for car in favorite_cars):
+    print("Porshe found")
+else:
+    print("Porshe not found")
