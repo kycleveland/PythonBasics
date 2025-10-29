e69e87e3751c83f85eebc831d11ab142b84235a8
Kyle Cleveland <kycleveland@gmail.com>
2025-10-28 19:26:39 -0500

    Creating Own Classes

---
+++ Functions_Classes/Functions/Function_Classes.py
@@ -0,0 +1,8 @@
+from classes.my_classes import Car
+
+# Creating the data for a car
+my_car = Car(make="Nissan", model="Titan SV", year=2021, mileage=92000, condition="Above Average", color="White")
+my_wifes_car = Car(make="Nissan", model="Rogue SL", year=2023, mileage=52000, condition="Great", color="White")
+
+print(my_car.make)
+print(my_wifes_car.__dict__)
